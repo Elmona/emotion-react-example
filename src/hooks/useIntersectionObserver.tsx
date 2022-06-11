acf57b8654b4704832f4fbcb414643d4ffa532e2
Anders Jonsson <aj224rj@student.lnu.se>
2022-06-11 23:39:14 +0200

    Update hook to use state

--- src/hooks/useIntersectionObserver.tsx
+++ src/hooks/useIntersectionObserver.tsx
@@ -1,19 +1,25 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 /**
  * Custom hook to use browser api Intersection Observer.
  *
  * @returns {object} - State-data.
  */
-const useIntersectionObserver = (target: HTMLElement, ancestor: HTMLElement, margin: number = 0, threshold: number = 1.0, callback: () => AnyTypeAnnotation) => {
+const useIntersectionObserver = (target: HTMLElement, ancestor: HTMLElement, margin: number = 0, threshold: number = 1.0) => {
+  const [isVisible, setIsVisible] = useState(false)
+
   const options = {
     root: ancestor ? ancestor : null,
     rootMargin: `${margin}px`,
     threshold
   }
   
-  const observer = new IntersectionObserver(callback, options);
+  const observer = new IntersectionObserver(() => setIsVisible(true), options);
   observer.observe(target)
+
+  return {
+    isVisible
+  }
 }
 
 export default useIntersectionObserver

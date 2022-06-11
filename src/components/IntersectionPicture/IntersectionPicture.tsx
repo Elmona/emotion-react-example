32d2e896606926e0d5d47b7f35aab6cf06ad644d
Anders Jonsson <aj224rj@student.lnu.se>
2022-06-11 21:33:50 +0200

    Add files for component and css

---
+++ src/components/IntersectionPicture/IntersectionPicture.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+/**
+ * Lazyloaded picture-component.
+ *
+ * @returns {JSX} - Picture-element with image-srcets.
+ */
+const IntersectionPicture : React.FC = () => {
+  console.log('loaded')
+  return (
+    <div>IntersectionPicture</div>
+  )
+}
+
+export default IntersectionPicture

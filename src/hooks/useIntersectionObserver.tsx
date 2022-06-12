dcaf7495b915c25719c50f64031a5929b089c170
Anders Jonsson <aj224rj@student.lnu.se>
2022-06-12 14:00:45 +0200

    Set up options as optional param to hook

--- src/hooks/useIntersectionObserver.tsx
+++ src/hooks/useIntersectionObserver.tsx
@@ -1,19 +1,24 @@
 import { useState, useEffect, useRef } from 'react'
-// TODO: Fix types.
+
+interface intersectionObserverOptions {
+  root: HTMLElement | null
+  rootMargin: string
+  threshold: number
+}
 
 /**
  * Custom hook to use browser api Intersection Observer.
  *
  * @returns {object} - Ref for target element and isIntersecting-state.
  */
-const useIntersectionObserver = (margin: number = 0, threshold: number = 1.0) => {
+const useIntersectionObserver = (options?: intersectionObserverOptions) => {
   const [isIntersecting, setIsIntersecting] = useState(false)
   const targetRef = useRef(null)
 
-  const options = {
-    root: null,
-    rootMargin: `${margin}px`,
-    threshold
+  const intersectionOptions = {
+    root: options?.root || null,
+    rootMargin: options?.rootMargin || '0px',
+    threshold: options?.threshold || 0
   }
 
   /**
@@ -33,7 +38,7 @@ const useIntersectionObserver = (margin: number = 0, threshold: number = 1.0) =>
   }
 
   useEffect(() => {
-  const observer = new IntersectionObserver(intersectionHandler, options)
+  const observer = new IntersectionObserver(intersectionHandler, intersectionOptions)
 
   if (targetRef.current) {
     observer.observe(targetRef.current)

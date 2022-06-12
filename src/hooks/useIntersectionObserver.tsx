3af073ee64b3e6298c2644cbc58919a661afd69e
Anders Jonsson <aj224rj@student.lnu.se>
2022-06-12 08:32:18 +0200

    Add callback-function to set isIntersecting

--- src/hooks/useIntersectionObserver.tsx
+++ src/hooks/useIntersectionObserver.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+// TODO: Fix types.
 
 /**
  * Custom hook to use browser api Intersection Observer.
@@ -6,19 +7,36 @@ import { useState } from 'react'
  * @returns {object} - State-data.
  */
 const useIntersectionObserver = (target: HTMLElement, ancestor: HTMLElement, margin: number = 0, threshold: number = 1.0) => {
-  const [isVisible, setIsVisible] = useState(false)
+  const [isIntersecting, setIsIntersecting] = useState(false)
 
   const options = {
     root: ancestor ? ancestor : null,
     rootMargin: `${margin}px`,
     threshold
   }
+
+  /**
+   * Callback-function checking if element is intersecting.
+   *
+   * @param entries - List of IntersectionObserverEntry-objects.
+   * @param observer - The observer.
+   */
+  const intersectionHandler = (entries, observer) => {
+    entries.forEach(entry => {
+      console.log(entry)
+      if (entry.isIntersecting) {
+        setIsIntersecting(true)
+
+        observer.unobserve(entry.target)
+      }
+    })
+  }
   
-  const observer = new IntersectionObserver(() => setIsVisible(true), options);
+  const observer = new IntersectionObserver(intersectionHandler, options);
   observer.observe(target)
 
   return {
-    isVisible
+    isIntersecting
   }
 }
 
